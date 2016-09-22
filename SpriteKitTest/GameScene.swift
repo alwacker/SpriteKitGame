b9437cfcafb9ee8a3980a7170e1f1109e8a68bd7
Oleksandr Vaker <oleksandrvaker@Oleksandrs-MacBook-Pro.local>
2016-09-22 20:34:47 +0200

    SpriteKitGame

--- SpriteKitTest/GameScene.swift
+++ SpriteKitTest/GameScene.swift
@@ -11,79 +11,108 @@ import GameplayKit
 
 class GameScene: SKScene {
     
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
+    //Texture
+    var bgTexture: SKTexture!
+    var flyHeroTex: SKTexture!
+
+    //Sprite Nodes
+    var bg = SKSpriteNode()
+    var ground = SKSpriteNode()
+    var hero = SKSpriteNode()
+    
+    //Sprite Objects
+    var bgObject = SKNode()
+    var groundObject = SKNode()
+    var heroObject = SKNode()
+    
+    //Bit masks
+    var heroGroup: UInt32 = 0x1 << 1
+    var groundGroup: UInt32 = 0x1 << 2
+    
+    //Textures Array for animateWithTextures
+    var heroFlyTexturesArray = [SKTexture]()
     
     override func didMove(to view: SKView) {
+        //BG texture
+        bgTexture = SKTexture(imageNamed: "bg01.png")
         
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
-        }
+        //Hero texture
+        flyHeroTex = SKTexture(imageNamed: "Fly0.png")
         
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
+        createObjects()
+        createGame()
+    }
+    
+    func createObjects(){
+        self.addChild(bgObject)
+        self.addChild(groundObject)
+        self.addChild(heroObject)
+    }
+    
+    func createGame(){
+        createBg()
+        createGround()
+        createHero()
         
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
+    }
+    
+    func createBg(){
+        bgTexture = SKTexture(imageNamed: "bg01.png")
+        
+        let moveBg = SKAction.moveBy(x: -bgTexture.size().width, y: 0, duration: 3)
+        let replaceBg = SKAction.moveBy(x: bgTexture.size().width, y: 0, duration: 0)
+        let moveBgForever = SKAction.repeatForever(SKAction.sequence([moveBg,replaceBg]))
+        
+        for i in 0..<3{
+            bg = SKSpriteNode(texture: bgTexture)
+            bg.position = CGPoint(x: size.width/4 + bgTexture.size().width * CGFloat(i), y: size.height/2.0)
+            bg.size.height = self.frame.height
+            bg.run(moveBgForever)
+            bg.zPosition = -1
             
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(M_PI), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
+            bgObject.addChild(bg)
         }
     }
     
-    
-    func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
-    }
-    
-    func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
-    }
-    
-    func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
-    }
-    
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
-        }
+    func createGround(){
+        ground = SKSpriteNode()
+        ground.position = CGPoint.zero
+        ground.physicsBody = SKPhysicsBody(rectangleOf: CGSize(width: self.frame.width, height: self.frame.height/4 + self.frame.height/8))
+        ground.physicsBody?.isDynamic = false
+        ground.physicsBody?.categoryBitMask = groundGroup
+        ground.zPosition = 1
         
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
+        groundObject.addChild(ground)
     }
     
-    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
+    func addHero(heroNode: SKSpriteNode, atPosition position: CGPoint){
+        hero = SKSpriteNode(texture: flyHeroTex)
+        
+        //Anim hero
+        heroFlyTexturesArray = [SKTexture(imageNamed: "Fly0.png"),SKTexture(imageNamed: "Fly1.png"),SKTexture(imageNamed: "Fly2.png"),SKTexture(imageNamed: "Fly3.png"),SKTexture(imageNamed: "Fly4.png")]
+        let heroFlyAnimation = SKAction.animate(with: heroFlyTexturesArray, timePerFrame: 0.1)
+        let flyHero = SKAction.repeatForever(heroFlyAnimation)
+        hero.run(flyHero)
+        
+        hero.position = position
+        
+        hero.size.height = 84
+        hero.size.width = 120
+        
+        hero.physicsBody = SKPhysicsBody(rectangleOf: CGSize(width: hero.size.width - 40, height: hero.size.height - 30))
+        
+        hero.physicsBody?.categoryBitMask = heroGroup
+        hero.physicsBody?.contactTestBitMask = groundGroup
+        hero.physicsBody?.contactTestBitMask = groundGroup
+        
+        hero.physicsBody?.isDynamic = true
+        hero.physicsBody?.allowsRotation = false
+        hero.zPosition = 1
+        
+        heroObject.addChild(hero)
     }
     
-    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    
-    override func update(_ currentTime: TimeInterval) {
-        // Called before each frame is rendered
+    func createHero(){
+        addHero(heroNode: hero, atPosition: CGPoint(x: self.size.width/4, y: 0 + flyHeroTex.size().height + 400))
     }
 }
---
+++ SpriteKitTest/GameScene_Touches.swift
@@ -0,0 +1,18 @@
+//
+//  GameScene_Touches.swift
+//  SpriteKitTest
+//
+//  Created by Oleksandr Vaker on 17/09/16.
+//  Copyright © 2016 Oleksandr Vaker. All rights reserved.
+//
+
+import Foundation
+import SpriteKit
+
+extension GameScene{
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+    }
+    
+}
